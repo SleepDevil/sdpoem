93d98e5a3ea4f0c2de4177d076d1dbee4b3c565b
SleepDevil <1752915704@qq.com>
2022-05-27 17:54:04 +0800

    add swiper

--- app/src/main/java/com/example/sdpoem/ui/write/WriteViewModel.java
+++ app/src/main/java/com/example/sdpoem/ui/write/WriteViewModel.java
@@ -1,14 +1,14 @@
-package com.example.sdpoem.ui.notifications;
+package com.example.sdpoem.ui.write;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class WriteViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public WriteViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is notifications fragment");
     }
